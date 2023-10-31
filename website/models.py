4620dbd1ed21276a9e44c395f14e2b86bbc6bd36
Phoebe Jackson <80435070+phoebejackson137@users.noreply.github.com>
2023-10-31 03:55:46 +0000

    basic DB tables & setup landing page

--- website/models.py
+++ website/models.py
@@ -1,19 +1,31 @@
 """
 Data structures given in the assessment folder
 """
+from . import db
 from datetime import datetime
 from flask_login import UserMixin
-from . import db
-
 
 class User(db.Model, UserMixin):
-    ""
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)#should be 128 in length to store hash
+    
+    usertype = db.Column(db.String(20), nullable=False, default='guest')
+
+
+    def __repr__(self):
+        return "<Name: {}, id: {}>".format(self.name, self.id)
 
 class Event(db.Model):
-    ""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), index=True, unique=False, nullable=False)
+    location = db.Column(db.String(250), nullable=False)
 
 class Comment(db.Model):
-    ""
+    id = db.Column(db.Integer, primary_key=True)
+   
 
 class Order(db.Model):
-    ""
+    id = db.Column(db.Integer, primary_key=True)
--- website/views.py
+++ website/views.py
@@ -1,11 +1,16 @@
 """
 Determines what content is in the viewport
 """
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
     """Landing Page"""
-    return '<h1>Starter code for the assessment<h1>'
+    return render_template('index.html')
+
+@main_bp.route('/create-event')
+def create_event():
+    """Event Creation Page"""
+    return render_template('event-update-or-create.html')
