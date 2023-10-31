352b6f909eca9d71e7b9298fa47deaf04018216a
Phoebe Jackson <80435070+phoebejackson137@users.noreply.github.com>
2023-10-31 13:29:35 +0000

    added more forms

--- website/forms.py
+++ website/forms.py
@@ -2,21 +2,43 @@
 Flask forms
 """
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.fields import (
+    FormField, SelectField, IntegerField, DecimalField,TextAreaField,
+    SubmitField, StringField, PasswordField, DateTimeField, FileField)
+from wtforms.validators import InputRequired, Email, EqualTo
 
-#creates the login information
+class TelephoneForm(FlaskForm):
+    """Directly from the Flask wtf forms documentation - recommended to store phone numbers"""
+    country_code = IntegerField('Country Code', validators=[InputRequired()])
+    area_code    = IntegerField('Area Code/Exchange', validators=[InputRequired()])
+    number       = StringField('Number')
+
+class EventForm(FlaskForm):
+    """Create new event"""
+    name = StringField('Country', validators=[InputRequired()])
+    description = TextAreaField('Description', validators = [InputRequired()])
+    image = FileField('Cover Image', validators=[InputRequired()])
+    start_time = DateTimeField('Start time', validators=[InputRequired()])
+    end_time = DateTimeField('End time', validators=[InputRequired()])
+    event_status = SelectField('Status', choices=['Open', 'Inactive', 'Sold Out', 'Cancelled'])
+    submit = SubmitField("Create")
+
+class OrderForm(FlaskForm):
+    """Order tickets to an event"""
+    num_tickets = IntegerField('Number of tickets', validators=[InputRequired()])
+    price_per_ticket = DecimalField('Price per ticket')
+
 class LoginForm(FlaskForm):
     """Login for existing users"""
     user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
     password=PasswordField("Password", validators=[InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
- # this is the registration form
 class RegisterForm(FlaskForm):
     """User registration"""
     user_name=StringField("User Name", validators=[InputRequired()])
-    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
+    user_email = StringField("Email Address", validators=[Email("Please enter a valid email")])
+    mobile_number = FormField(TelephoneForm)
     #linking two fields - password should be equal to data entered in confirm
     password=PasswordField("Password", validators=[InputRequired(),
                   EqualTo('confirm', message="Passwords should match")])
@@ -24,7 +46,6 @@ class RegisterForm(FlaskForm):
 
     #submit button
     submit = SubmitField("Register")
-
 
 class CommentForm(FlaskForm):
     """ User comments on events """
