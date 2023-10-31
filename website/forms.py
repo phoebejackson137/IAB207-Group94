8413c001471058f5ab76a31fba2cd9924c499e7e
Phoebe Jackson <80435070+phoebejackson137@users.noreply.github.com>
2023-10-31 13:02:54 +0000

    update forms

--- website/forms.py
+++ website/forms.py
@@ -1,15 +1,20 @@
+"""
+Flask forms
+"""
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 
 #creates the login information
 class LoginForm(FlaskForm):
+    """Login for existing users"""
     user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
     password=PasswordField("Password", validators=[InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
  # this is the registration form
 class RegisterForm(FlaskForm):
+    """User registration"""
     user_name=StringField("User Name", validators=[InputRequired()])
     email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
     #linking two fields - password should be equal to data entered in confirm
@@ -19,3 +24,9 @@ class RegisterForm(FlaskForm):
 
     #submit button
     submit = SubmitField("Register")
+
+
+class CommentForm(FlaskForm):
+    """ User comments on events """
+    text = TextAreaField('Comment', [InputRequired()])
+    submit = SubmitField('Create')
