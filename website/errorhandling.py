180318698f47b1df3e9a204bd667d998bc4da51c
SVQUT <149133198+SVQUT@users.noreply.github.com>
2023-11-10 22:41:13 +1000

    error handling notes

--- website/errorhandling.py
+++ website/errorhandling.py
@@ -12,42 +12,42 @@ def not_found_error(error):
 def server_error(error):
     return render_template('500.html'), 500
 
-# Custom error page for 501 - Internal Server Error
+# Custom error page for 501 - Not Implemented
 @app.errorhandler(501)
 def server_error(error):
     return render_template('501.html'), 501
 
-# Custom error page for 502 - Internal Server Error
+# Custom error page for 502 - Bad Gateway
 @app.errorhandler(502)
 def server_error(error):
     return render_template('502.html'), 502
 
-# Custom error page for 503 - Internal Server Error
+# Custom error page for 503 - Service Unavailable
 @app.errorhandler(503)
 def server_error(error):
     return render_template('503.html'), 503
 
-# Custom error page for 504 - Internal Server Error
+# Custom error page for 504 - Gateway Timeout
 @app.errorhandler(504)
 def server_error(error):
     return render_template('504.html'), 504
 
-# Custom error page for 505 - Internal Server Error
+# Custom error page for 505 - HTTP Version Not Supported
 @app.errorhandler(505)
 def server_error(error):
     return render_template('505.html'), 505
 
-# Custom error page for 506 - Internal Server Error
+# Custom error page for 506 - Variant Also Negotiates
 @app.errorhandler(506)
 def server_error(error):
     return render_template('506.html'), 506
 
-# Custom error page for 507 - Internal Server Error
+# Custom error page for 507 - Insufficient Storage
 @app.errorhandler(507)
 def server_error(error):
     return render_template('507.html'), 507
 
-# Custom error page for 508 - Internal Server Error
+# Custom error page for 508 - Loop Detected
 @app.errorhandler(508)
 def server_error(error):
     return render_template('508.html'), 508
